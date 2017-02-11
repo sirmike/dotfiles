03c9459c688b1acf185e8eed19a961495d74477b
SirMike <mail@sirmike.org>
2017-02-11 22:22:09 +0100

    Add script for Vim essential plugins installation

---
+++ vim/install_plugins.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ~/.vim/bundle
+git clone git://github.com/rking/ag.vim
+git clone git://github.com/kien/ctrlp.vim.git
+git clone git://github.com/vim-airline/vim-airline
+git clone git://github.com/kchmck/vim-coffee-script.git
+git clone git://github.com/altercation/vim-colors-solarized.git
+git clone git://github.com/easymotion/vim-easymotion
+git clone git://github.com/christoomey/vim-tmux-navigator
+git clone git://github.com/thoughbot/vim-rspec.git
+git clone git://github.com/tpope/vim-commentary.git
+git clone git://github.com/tpope/vim-dispatch.git
+git clone git://github.com/tpope/vim-endwise.git
+git clone git://github.com/tpope/vim-fugitive.git
+git clone git://github.com/tpope/vim-sensible.git
+git clone git://github.com/tpope/vim-surround.git
+git clone git://github.com/tpope/vim-unimpaired.git
+git clone git://github.com/tpope/vim-vinegar.git
